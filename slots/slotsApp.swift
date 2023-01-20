cb5bb2fd13f4a181a3e1193cccb66acaf3a0dee6
gravilaxe <joshuawright3132@outlook.com>
2023-01-20 21:49:32 +0000

    Initial Commit

---
+++ slots/slotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  slotsApp.swift
+//  slots
+//
+//  Created by Joshua Wright on 20/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct slotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
